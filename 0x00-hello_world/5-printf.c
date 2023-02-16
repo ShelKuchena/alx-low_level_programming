66ea7db3f664a819e167858a9c02e0094b4b72c8
ShelKuchena <shelterkuchena@gmail.com>
2023-02-16 15:32:19 -0800

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using the printf function
- * Return: 0 (Success)
+ * main - main block
+ * Return: o
  */
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,⧵n")
-return (0);
-}	
+}
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0)
