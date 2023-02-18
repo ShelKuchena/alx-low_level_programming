54536bb025f7529c54e510447d26239757dfcf2d
ShelKuchena <shelterkuchena@gmail.com>
2023-02-17 16:40:57 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
  * main - main block
- * Description: get a random number and check its
+ * Description: prints all single digit number of base 10
+ * starting from 0, followed by new line.
  * Return: 0
  */
 int main(void)
 { 
-	char c = 'a;;
-	while (c <= 'z')
-	{
-		if (c != 'q')
+	char c = '0';
+
+	while (c <= '9')
+	{ 
 	        
 		putchar(c);
 	        } 
