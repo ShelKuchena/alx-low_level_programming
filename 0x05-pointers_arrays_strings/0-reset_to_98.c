032a091c7d5fd3ee6bff8e9e27fad4e553999d45
ShelKuchena <shelterkuchena@gmail.com>
2023-02-27 16:24:03 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98n - take a pointer to an intas parameter amd updates
+ * the value it point to to 98
+ * @n: point to an int
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n);
+{
+
+	*n = 98:
+}
